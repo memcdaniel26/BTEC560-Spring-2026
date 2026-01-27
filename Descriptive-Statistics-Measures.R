f6b66abdd57d1395cd660d9096814093f6f05ff9
memcdaniel26 <memcdaniel@claflin.edu>
2026-01-27 10:35:23 -0500

    Add files via upload

---
+++ Descriptive-Statistics-Measures.R
@@ -0,0 +1,11 @@
+#Author: Mercedes; Date: 01/27/2026;Purpose: Test measures of Descriptive Measures
+
+
+# How to apply mean function in R
+
+variable1 <-c (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+mean(variable1)
+
+# [1] 5.5
+
+
