946a3673a56d34fe22a2eab5ea5824f3941d811e
memcdaniel26 <memcdaniel@claflin.edu>
2026-02-05 10:28:12 -0500

    Add files via upload

---
+++ Descriptive-Measures.R
@@ -0,0 +1,18 @@
+#Author: Mercedes, Date: 02/05/2026, Purpose: Test descriptive measures
+
+#create variable called "variable new" holding 100 variables from 100 to 200
+#calculate mean on variablenew for points 150 to 200
+#calculate median on variablenew
+#calculate mean on variable on variablenew for points 100 to 150
+
+Save the outputs after each function in this value, upload this file in your github repository to get full points.
+
+
+#variablenew <- 100:200
+mean_150_200 <- mean(variablenew[150:200])
+median_all<- median(variablenew)
+mean_100_150 <- mean(variablenew[100:150])
+
+mean_150_200
+median_all
+mean_100_150
